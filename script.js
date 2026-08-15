f347b15bc098a5d5b51cd8db6936f4d1599ae844
Javohir <zavohirzumaev1@gmail.com>
2026-08-16 00:22:47 +0500

    update

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const display = document.getElementById("display");
 
 function add(input){
-    display.value += input;
+    display.value += input; // добавляем полученное значение к тому, что уже находится в display
 }
 
 function clearDisplay(){
-    display.value = "";
+    display.value = ""; // очищаем display
 }
 
 function calculate(){
-    display.value = eval(display.value);
+    display.value = eval(display.value); // считаем все значения display
 }
